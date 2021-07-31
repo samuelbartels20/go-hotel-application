ffe6acc8649cdf871e98f09876e06f9a943b6af6
samuelbartels20 <bartelssamuel20@gmail.com>
2021-07-31 00:16:06 +0000

    refactored the code to include functions

--- hello.go
+++ hello.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+   "fmt"
+   "math/rand"
+   "time"
 )
 
 func main() {
@@ -11,38 +11,49 @@ func main() {
    const totalRooms = 134
    const firstRoomNumber = 110
 
-   rand.Seed(time.Now().UnixNano())
+   rand.Seed(time.Now().UTC().UnixNano())
    roomsOccupied := rand.Intn(totalRooms)
-
    roomsAvailable := totalRooms - roomsOccupied
 
-   occupancyRate := (float64(roomsOccupied) / float64(totalRooms)) * 100
+   occupancyRate := occupancyRate(roomsOccupied, totalRooms)
+   occupancyLevel := occupancyLevel(occupancyRate)
 
-   var occupancyLevel string
-   if occupancyRate > 70 {
-	   occupancyLevel = "High"
-   } else if  occupancyRate > 20 {
-		occupancyLevel = "Medium"
-   } else {
-	   occupancyLevel = "Low"
-   }
-
-   fmt.Println("Hotel: ", hotelName)
+   fmt.Println("Hotel:", hotelName)
    fmt.Println("Number of rooms", totalRooms)
    fmt.Println("Rooms available", roomsAvailable)
-   fmt.Println("                   Occupancy Level: ", occupancyLevel)
-   fmt.Printf("                    Occupancy Rate: %0.2f %%\n", occupancyRate)
+   fmt.Println("                  Occupancy Level:", occupancyLevel)
+   fmt.Printf("                  Occupancy Rate: %0.2f %%\n", occupancyRate)
 
    if roomsAvailable > 0 {
-	   fmt.Println("Rooms:")
-	   for i :=0; roomsAvailable > i; i++ {
-		   roomNumber := firstRoomNumber + i
-		   size := rand.Intn(6) + 1
-		   nights := rand.Intn(10) + 1
-		   fmt.Println(roomNumber, ":", size, "people /", nights, " nights")
-	   }
+      fmt.Println("Rooms:")
+      for i := 0; roomsAvailable > i; i++ {
+         roomNumber := firstRoomNumber + i
+         size := rand.Intn(6) + 1
+         nights := rand.Intn(10) + 1
+         printRoomDetails(roomNumber, size, nights)
+      }
    } else {
-	   fmt.Println("No Rooms Available for tonight")
+      fmt.Println("No rooms available for tonight")
+   }
+
+}
+
+
+func printRoomDetails(roomNumber, size, nights int){
+   fmt.Println(roomNumber, ":", size, nights, "nights" )
+}
+
+func occupancyLevel(occupancyRate float32) string{
+   if occupancyRate > 70 {
+      return "High"
+   } else if occupancyRate > 20 {
+      return "Medium"
+   } else {
+      return "Low"
    }
 }
 
+func occupancyRate(roomsOccupied int, totalRooms int) float32 {
+   return (float32(roomsOccupied) / float32(totalRooms)) *100
+}
+   
